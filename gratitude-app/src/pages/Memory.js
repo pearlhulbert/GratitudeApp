cc67d779632092819ffd2832b88ad0798c2787f2
marshnoe000 <noelle.marshall17@gmail.com>
2022-12-06 23:34:04 +0000

    fixed timestamp

--- gratitude-app/src/pages/Memory.js
+++ gratitude-app/src/pages/Memory.js
@@ -4,12 +4,13 @@ import {db} from '../firebaseConfig.js';
 import { doc, deleteDoc } from "firebase/firestore";
 import '../todo.css';
 
+const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
 const Memory=({arr})=>{
     return (
         <List className="todo__list">
             <ListItem>
             <ListItemAvatar />
-                <ListItemText primary={arr.item.timestamp.toDate().toString()} secondary={arr.item.todo} />
+                <ListItemText primary={arr.item.timestamp.toDate().toLocaleDateString('en-us', options)} secondary={arr.item.todo} />
             </ListItem>
             <DeleteIcon fontSize="large" style={ {opacity:0.7} } onClick={() => {deleteDoc(doc(db, 'memories', arr.id))}} />
         </List>
