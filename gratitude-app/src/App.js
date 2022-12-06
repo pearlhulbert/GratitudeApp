29f3b20bd48decd2e5bd75d71320566ae18dcfea
Ubuntu <ubuntu@ip-172-31-16-80.us-west-1.compute.internal>
2022-12-06 21:01:17 +0000

    fixed layout (mostly)

--- gratitude-app/src/App.js
+++ gratitude-app/src/App.js
@@ -1,20 +1,45 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout.js";
-import Journal from "./pages/Journal.js";
-import Memories from "./pages/Memories.js";
 import './App.css';
+import React, {useInsertionEffect, useState} from 'react';
+import {TextField , Button } from '@mui/material';
+import Memory from './pages/Memory';
+import { db } from './firebaseConfig.js';
+import { collection, query, onSnapshot, serverTimestamp, addDoc } from 'firebase/firestore';
 
+//const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'));
+function App() {
+  const [todos, setTodos]=useState([]);
+  const [input, setInput]=useState('');
 
-export default function App() {
+  useInsertionEffect(() => {
+    onSnapshot(collection(db, 'memories'), (snapshot)=> {
+      setTodos(snapshot.docs.map(doc => ({
+        id: doc.id,
+        item: doc.data()
+      })))
+    })
+  }, [input]);
+
+  const addTodo=(e)=>{
+    e.preventDefault();
+    addDoc(collection(db, 'memories'), {
+      todo:input,
+      timestamp: serverTimestamp()
+  })
+    setInput('')
+  };
+
   return (
-    <BrowserRouter basename="/GratitudeApp/gratitude-app/build">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Journal />} />
-          <Route path="memories" element={<Memories />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <h2> TODO List App</h2>
+      <form>
+        <TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{margin:"0px 5px"}} size="small" value={input}
+        onChange={e=>setInput(e.target.value)} />
+        <Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
+      </form>
+       <ul>
+          {todos.map(item => <Memory key = {item.id} arr={item} />)}
+        </ul>
+    </div>
   );
 }
+export default App;
