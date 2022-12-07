8762c416b15a9e93d5bb1b642d48024c3407ce3c
Ubuntu <ubuntu@ip-172-31-16-80.us-west-1.compute.internal>
2022-12-07 22:09:58 +0000

    finsihed

--- gratitude-app/src/App.js
+++ gratitude-app/src/App.js
@@ -6,40 +6,42 @@ import { db } from './firebaseConfig.js';
 import { collection, query, onSnapshot, serverTimestamp, addDoc, orderBy } from 'firebase/firestore';
 
 function App() {
-  const [todos, setTodos]=useState([]);
+  const [entries, setEntries]=useState([]);
   const [input, setInput]=useState('');
   const q = query(collection(db, 'memories'), orderBy('timestamp', 'desc'));
 
   useInsertionEffect(() => {
     onSnapshot(q, (snapshot)=> {
-      setTodos(snapshot.docs.map(doc => ({
+      setEntries(snapshot.docs.map(doc => ({
         id: doc.id,
         item: doc.data()
       })))
     })
   }, [input]);
 
-  const addTodo=(e)=>{
+  const addEntry=(e)=>{
     e.preventDefault();
     addDoc(collection(db, 'memories'), {
-      todo:input,
+      entry:input,
       timestamp: serverTimestamp()
   })
     setInput('')
   };
 
+
   return (
     <div className="App">
+    
       <div id = "header">
       <h1>Personal Journal</h1>
       </div>
       <form>
         <TextField id="outlined-basic" label="Create Journal Entry" variant="outlined" style={{margin:"0px 5px"}} size="small" value={input}
         onChange={e=>setInput(e.target.value)} />
-        <Button variant="contained" color="primary" onClick={addTodo}  >Add Entry</Button>
+        <Button variant="contained" color="primary" onClick={addEntry}  >Add Entry</Button>
       </form>
        <ul>
-          {todos.map(item => <Memory key = {item.id} arr={item} />)}
+          {entries.map(item => <Memory key = {item.id} arr={item} />)}
         </ul>
                     <footer>
         <p>Github Repository: <a href="https://github.com/pearlhulbert/GratitudeApp">https://github.com/pearlhulbert/GratitudeApp</a></p>
--- gratitude-app/src/pages/Memory.js
+++ gratitude-app/src/pages/Memory.js
@@ -1,16 +1,25 @@
 import { List , ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {db} from '../firebaseConfig.js';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc} from "firebase/firestore";
 import '../todo.css';
 
-const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
 const Memory=({arr})=>{
+    
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    
+    let currDate = arr.item.timestamp;
+    let dateString = "Now";
+    if (currDate != null) {
+        dateString = currDate.toDate().toLocaleDateString('en-us', options);
+    }
+    
+    
     return (
         <List className="todo__list">
             <ListItem>
             <ListItemAvatar />
-                <ListItemText primary= {arr.item.todo} secondary={arr.item.todo} />
+                <ListItemText primary={dateString} secondary={arr.item.entry} />
             </ListItem>
             <DeleteIcon fontSize="large" style={ {opacity:0.7} } onClick={() => {deleteDoc(doc(db, 'memories', arr.id))}} />
         </List>
